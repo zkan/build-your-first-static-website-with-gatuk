2aaadd995c7dce1f4a0c155f6fd1f7145c23ae13
zkan <zkan.cs@gmail.com>
2021-05-29 10:47:47 +0700

    Print values in variable :bear:

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Hello, JavaScript!")
+
+var name = "Kan"
+console.log("Name:", name)
+
+var age = 20
+console.log("Age:", age)
+
+var hasComputer = true
+console.log("hasComputer:", hasComputer)
+
+console.log(`My name is ${name} and I'm ${age} years old`)
