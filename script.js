c3917bc80bb17f3c5412aedd99eaf88e89014ae1
zkan <zkan.cs@gmail.com>
2021-05-29 10:59:57 +0700

    Create function to calculate sum of 2 values :bear:

--- script.js
+++ script.js
@@ -10,3 +10,10 @@ var hasComputer = true
 console.log("hasComputer:", hasComputer)
 
 console.log(`My name is ${name} and I'm ${age} years old`)
+
+function add(a, b) {
+    return a + b
+}
+
+var result = add(3, 10)
+console.log("Result:", result)
